e1bb501fcb3e615d737d71ec1357960f6aa2ad37
An Bin <quocthienhong@gmail.com>
2023-10-15 18:29:18 +0700

    push cart and cartItem

--- js/controller/main.js
+++ js/controller/main.js
@@ -1,4 +1,6 @@
 var api = new CallApi();
+var products = [];
+const cart = new Cart();
 
 function getEle(id) {
   return document.getElementById(id);
@@ -18,6 +20,7 @@ function getListProduct() {
 getListProduct();
 
 function renderProduct(data) {
+  products = data;
   var content = "";
 
   for (var i = 0; i < data.length; i++) {
@@ -42,7 +45,7 @@ function renderProduct(data) {
             <i class="fa-solid fa-star"></i>
             <i class="fa-solid fa-star"></i>
           </div>
-          <button class="buy_now">Add to cart</button>
+          <button class="buy_now" onclick="addToCart('${product.id}')">Add to cart</button>
         </div>
       </div>
         
@@ -80,3 +83,90 @@ var brandCheckboxes = document.querySelectorAll('.brand_name');
 brandCheckboxes.forEach(function (checkbox) {
   checkbox.addEventListener('change', filterProductsByBrand);
 });
+
+// tạo giỏ hàng
+function addToCart(id) {
+  // Tìm sản phẩm trong giỏ hàng dựa trên id
+  const existingItem = cart.list.find((item) => item.id === id);
+
+  if (existingItem) {
+    // Nếu sản phẩm đã tồn tại trong giỏ hàng, tăng số lượng lên 1 đơn vị
+    cart.increaseQuantity(id);
+  } else {
+    const existingProduct = products.find((item) => item.id === id);
+    // Nếu sản phẩm chưa có trong giỏ hàng, tạo đối tượng mới và thêm vào giỏ hàng
+    const cartItem = new CartItem(existingProduct.id, existingProduct.name, existingProduct.price, existingProduct.img, 1);
+    cart.themSP(cartItem);
+  }
+  // console.log(cart.list);
+  renderCart();
+}
+
+function renderCart() {
+  var contentItem = "";
+  var contentTotal = "";
+  var total = 0;
+
+  for (var i = 0; i < cart.list.length; i++) {
+    var cartItem = cart.list[i];
+
+    var itemPrice = cartItem.price * cartItem.quantity;
+    total += itemPrice;
+
+    contentItem += `
+      <tr>
+        <td>
+          <img src="${cartItem.img}">
+          <span>${cartItem.name}</span>
+        </td>
+        <td><span>${cartItem.price}</span></td>
+        <td>
+          <button class="btn-quantity btn-success btn-increase" onclick="changeQuantity('${cartItem.id}', 'increase')">+</button>
+          <span>${cartItem.quantity}</span>
+          <button class="btn-quantity btn-danger btn-decrease" onclick="changeQuantity('${cartItem.id}', 'decrease')">-</button>        
+        </td>
+        <td><button id="cart-delete" class="btn btn-danger" onclick="deleteProduct('${cartItem.id}')">Remove</button></td>
+      </tr>
+    `
+  }
+  
+  contentTotal += `
+    <p style="font-weight: bold;">Total: <span id="cart_total">${total}</span></p>
+    <button class="checkout-button btn btn-success" onclick="checkOutCart()">Checkout</button>
+  `
+  getEle("tbodyCart").innerHTML = contentItem;
+  getEle("cart-summary").innerHTML = contentTotal;
+}
+
+// remove sản phẩm
+function deleteProduct(id) {
+  const index = cart.list.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    cart.list.splice(index, 1); 
+    renderCart(); 
+  }
+}
+
+// tăng giảm sản phẩm
+function changeQuantity(id, action) {
+  const product = cart.list.find((item) => item.id === id);
+
+  if (product) {
+    if (action === 'increase') {
+      product.quantity += 1;
+    } else if (action === 'decrease') {
+      if (product.quantity > 1) {
+        product.quantity -= 1;
+      } else {
+        deleteProduct(id);
+      }
+    }
+    renderCart(); 
+  }
+}
+
+// checkout sản phẩm
+function checkOutCart() {
+  cart.list = [];
+  renderCart();
+}
